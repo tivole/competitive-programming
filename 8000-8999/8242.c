587e26202c74327aa6fdc98797b0c3a7d6e78dfc
tivole <asgarovkamran@gmail.com>
2019-09-22 23:35:04 +0400

    Added 8242

---
+++ 8000-8999/8242.c
@@ -0,0 +1,14 @@
+// (c) Tivole
+
+// 8242
+
+#include <stdio.h>
+
+int main() {
+  int n;
+  scanf("%d", &n);
+  if(n > 0) printf("Positive");
+  else if(n < 0) printf("Negative");
+  else printf("Zero");
+  return 0;
+}
