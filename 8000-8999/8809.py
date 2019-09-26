620609a4a85c11645eb03702d76f1ba84aafa779
tivole <asgarovkamran@gmail.com>
2019-09-26 21:17:22 +0400

    Added 8809, 8810, 8811

---
+++ 8000-8999/8809.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8809
+
+a, b = list(map(int, input().split()))
+
+print(a - b)
---
+++ 8000-8999/8810.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8810
+
+a, b, c = list(map(int, input().split()))
+
+print(a + b - c)
