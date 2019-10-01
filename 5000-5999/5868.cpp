0003e9d2100d7f34cb0c719a9f0eacbc6df8e506
tivole <asgarovkamran@gmail.com>
2019-10-01 22:42:26 +0400

    Add 5868

---
+++ 5000-5999/5868.cpp
@@ -0,0 +1,19 @@
+// (c) Tivole
+
+// 5868
+
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+
+    scanf("%d%d", &a, &b);
+
+    printf("%d\n", a^b);
+
+    return 0;
+}
