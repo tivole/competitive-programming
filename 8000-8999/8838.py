903614c81d8ce43e9ae26a5fc4ee3a23ccc83c57
tivole <asgarovkamran@gmail.com>
2019-11-09 23:33:46 +0400

    Solved 8838, 8839

---
+++ 8000-8999/8838.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8838
+
+x, y = list(map(float, input().split()))
+
+print(int(x//y))
---
+++ 8000-8999/8839.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8839
+
+x, y = list(map(float, input().split()))
+
+print(int(x%y))
