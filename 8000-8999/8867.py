a40eff5f1f36b73bd896ebe7c8a326d4282fe8cb
tivole <asgarovkamran@gmail.com>
2020-02-07 17:02:42 +0400

    Solved 8867, 8868

---
+++ 8000-8999/8867.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8867
+
+x, y = list(map(int, input().split()))
+
+print(x if x < y else y)
---
+++ 8000-8999/8868.py
@@ -0,0 +1,7 @@
+# (c) Tivole
+
+# 8868
+
+x, y = list(map(int, input().split()))
+
+print(x if x > y else y)
