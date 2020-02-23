111937fd5c9ebcf720a70a12e8874d40c97920a9
tivole <asgarovkamran@gmail.com>
2020-02-23 06:09:39 +0400

    Solved 8885, 8886

---
+++ 8000-8999/8885.py
@@ -0,0 +1,10 @@
+# (c) Tivole
+
+# 8885
+
+i = int(input())
+
+if abs(i) % 2 == 0:
+    print(i - 1)
+else:
+    print(i - 2)
---
+++ 8000-8999/8886.py
@@ -0,0 +1,10 @@
+# (c) Tivole
+
+# 8886
+
+i = int(input())
+
+if abs(i) % 2 == 0:
+    print(i - 2)
+else:
+    print(i - 1)
