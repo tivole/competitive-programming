c8347aab9fd95c7db96728c90afb16f5ede6062b
tivole <asgarovkamran@gmail.com>
2020-07-07 22:56:41 +0400

    Solve 8949

---
+++ 8000-8999/8949.py
@@ -0,0 +1,15 @@
+# (c) Tivole
+
+# 8949
+
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if i + j <= n-1 and j >= i:
+            print('*', end='')
+        elif i + j >= n-1 and j <= i:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
